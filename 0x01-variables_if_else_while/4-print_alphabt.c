0afab6fe0a1bb7b6faa84caf35ab35ad32267683
jordanbsandoval <1608@holbertonschool.com>
2020-07-04 22:48:15 -0500

    hotfix

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
 
-/**
- * main - Print the alphabet less the letter e and q.
- *
- * Return: exit successful.
+/** 
+ *main - Print the alphabet less the letter e and q.
+ *Return: exit successful.
  */
 
 int main(void)
 {
-	char AlphabetLowerCase;
+    char c = 'a';
 
-	AlphabetLowerCase = 'a';
-	for (; AlphabetLowerCase != ('\n' + 1); ++AlphabetLowerCase)
-	{
-		if (AlphabetLowerCase == ('z' + 1))
-			AlphabetLowerCase = '\n';
-		if ((AlphabetLowerCase != 'e') && (AlphabetLowerCase != 'q'))
-			putchar(AlphabetLowerCase);
-	}
-	return (0);
+    for(; c <= 'z'; ++c)
+    {
+        if(c != 'e' && c != 'q')
+            putchar(c);
+    }
 }
