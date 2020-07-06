46ebf7fcca92bc5a4adaf44eca1131b81b926669
jordanbsandoval <1608@holbertonschool.com>
2020-07-05 22:55:20 -0500

    hotfix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,130 +1,51 @@
-#include "holberton.h"
 #include <stdio.h>
-
-#define MENSAJE "Error\n"
-#define NUMBERCOINS 5
-
-void print_number(int Number);
-int _atoi(char []);
+#include <stdlib.h>
 
 /**
- * main -  Program that prints the minimum number of coins to make change
- *         for an amount of money.
- * @argc:  Counter.
- * @argv:  Doble Pointer.
- * Return: If the number of arguments passed to your program is not
- *         exactly 1, print Error, followed by a new line, and return 1
+ * main - number of coins to make change for an amount of money
+ * @argc: argument count
+ * @argv: array of strings
+ * Return: 1 if error, 0 otherwise
  */
 
 int main(int argc, char *argv[])
 {
-	if (argc == 2)
-	{
-		int OfCoins_Of_Values[NUMBERCOINS] = {25, 10, 5, 2, 1}, Counter = 0, Number;
+	int t, c;
+	unsigned int i;
+	char *p;
+	int cents[] = {25, 10, 5, 2};
 
-		Number = _atoi(*++argv);
-		if (Number <= 0)
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	t = strtol(argv[1], &p, 10);
+	c = 0;
+
+	if (!*p)
+	{
+		while (t > 1)
 		{
-			printf("0\n");
-			return (0);
-		}
-		else
-		{
-			while (Number)
+			for (i = 0; i < sizeof(cents[i]); i++)
 			{
-				if ((Number - OfCoins_Of_Values[0]) >= 0)
-					Counter++, Number = Number - OfCoins_Of_Values[0];
-				else if ((Number - OfCoins_Of_Values[1]) >= 0)
-					Counter++, Number = Number - OfCoins_Of_Values[1];
-				else if ((Number - OfCoins_Of_Values[2]) >= 0)
-					Counter++, Number = Number - OfCoins_Of_Values[2];
-				else if ((Number - OfCoins_Of_Values[3]) >= 0)
-					Counter++, Number = Number - OfCoins_Of_Values[3];
-				else if ((Number - OfCoins_Of_Values[4]) >= 0)
-					Counter++, Number = Number - OfCoins_Of_Values[4];
+				if (t >= cents[i])
+				{
+					c += t / cents[i];
+					t = t % cents[i];
+				}
 			}
-			print_number(Counter);
-			putchar('\n');
-			return (0);
 		}
+		if (t == 1)
+			c++;
 	}
 	else
 	{
-		char *Pointer_To_Mensaje = MENSAJE;
-
-		while (*Pointer_To_Mensaje)
-			_putchar(*Pointer_To_Mensaje++);
+		printf("Error\n");
 		return (1);
 	}
-}
 
-#define POWER_OF_10    10
-
-/**
- * _atoi - Converts a string to a number.
- * @Pointer_To_Number: Pointer.
- * Return: Return the number.
- */
-
-int _atoi(char Pointer_To_Number[])
-{
-	int sing, Number = 0, State = 0;
-
-	do {
-		if (!State)
-			sing = (*Pointer_To_Number == '-') ? -1 : 1, State = 1;
-		if ((*Pointer_To_Number == '+') || (*Pointer_To_Number == '-'))
-			Pointer_To_Number++;
-
-		Number = POWER_OF_10 * Number + (*Pointer_To_Number - '0');
-	} while (*++Pointer_To_Number)
-		;
-
-	return (sing * Number);
-}
-
-
-/**
- * print_number - prints an integer char by char
- * @Number: The integer to print
- */
-
-void print_number(int Number)
-{
-	int  SolveCharacter;
-	signed int PowerOf10;
-	unsigned char State, State0;
-
-	State = PowerOf10 =  State0 = 1, SolveCharacter = 0;
-
-	while (PowerOf10)
-	{
-		if ((Number < 0) && State0)
-			SolveCharacter = '-', State0 = 0;
-		else if (((Number / PowerOf10) >= POWER_OF_10 ||
-			  (Number / PowerOf10 <= -10)) && (State))
-		{
-			PowerOf10 *= POWER_OF_10;
-			continue;
-		}
-		else if (State)
-		{
-			State = 0;
-			continue;
-		}
-		else if (POWER_OF_10)
-		{
-			if (Number >= 0)
-				SolveCharacter = (Number / PowerOf10) % POWER_OF_10 + '0';
-			else
-			{
-				SolveCharacter = Number / PowerOf10 % 10;
-				SolveCharacter *= (-1);
-				SolveCharacter += '0';
-			}
-			PowerOf10 /= POWER_OF_10;
-		}
-		if (SolveCharacter)
-			_putchar(SolveCharacter);
-	}
+	printf("%d\n", c);
+	return (0);
 }
