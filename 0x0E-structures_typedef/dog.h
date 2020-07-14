0803a15daf18bfbb83b097c29496512798fc31f4
jordanbsandoval <1608@holbertonschool.com>
2020-07-13 23:38:15 -0500

    hotfix

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/**
+ * struct dog - It's a dog, what can I say
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner name
+ *
+ * Description: It's a dog, what can I say
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
