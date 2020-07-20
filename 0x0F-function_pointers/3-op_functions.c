e311632cb65b3cd68dc7603f42512495db31a08b
jordanbsandoval <1608@holbertonschool.com>
2020-07-19 23:39:04 -0500

    hotfix

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of operation.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of operation.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of operation.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of operation.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - reminder two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of operation.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
