9673e60b8ec8547d7d3f2e9bc27a7b6526e08015
jordanbsandoval <1608@holbertonschool.com>
2020-07-26 23:57:20 -0500

    hotfix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,17 +1,58 @@
+#include <string.h>
 #include "lists.h"
 
 /**
- * list_len - Function that returns the number of elements in a
- *            linked list_t list
- * @Pointer_Running_List: Pointer running trought the linked list.
- * Return: List item numbers.
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: length of s
  */
 
-size_t list_len(const list_t *Pointer_Running_List)
+int _strlen(char *s)
 {
-	size_t Counter = 0;
 
-	while (Pointer_Running_List)
-		Counter++, Pointer_Running_List = Pointer_Running_List->next;
-	return (Counter);
+	int size;
+
+	size = 0;
+
+	while (s[size] != '\0')
+	{
+		size++;
+	}
+
+	return (size);
+}
+/**
+ * add_node -  list_t *add_node(list_t **head, const char *str);
+ *
+ * @head: head
+ * @str: string
+ * Return: list with new node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	char *s;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	s = strdup(str);
+	if (s == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->str = s;
+	new_node->len = _strlen(s);
+	if (*head)
+	{
+		new_node->next = *head;
+	}
+	else
+	{
+		new_node->next = NULL;
+	}
+	*head = new_node;
+	return (*head);
 }
