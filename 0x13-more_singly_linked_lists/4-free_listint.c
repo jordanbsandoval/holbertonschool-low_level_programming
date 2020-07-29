508d0ef25ad18c7797a6d175148dd209e723bd06
jordanbsandoval <1608@holbertonschool.com>
2020-07-28 23:31:44 -0500

    hotfix

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - Function for free memory list
+ * @head: listint_t type of node struct pointer to head
+ * Return: Always successful
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmp = NULL;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
