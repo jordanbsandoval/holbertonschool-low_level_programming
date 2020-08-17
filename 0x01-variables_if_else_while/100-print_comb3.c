f0200ddbfb70a31a7f0a3021711965288d7db91f
jordanbsandoval <1608@holbertonschool.com>
2020-08-16 23:47:36 -0500

    hotfix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,48 @@
 #include <stdio.h>
-
 /**
- *main- It's program print all possible combinations of two digits
- *      Numbers must be separated by ,, followed by a space
- *      The two digits must be different
- *      01 and 10 are considered the same combination of the two digits 0 and 1
- *Return: exit succesful
+ * main - program that prints all possible
+ *        different combinations of two digits.
+ * Return: exit successful.
  */
+
+#define VALUEINICIAL '0'    /* value at which the output begins.*/
+#define BREAKWHILE ('\n' + 1)  /* Cycle breaking condition */
+#define NUMBER9 '9'   /* It controls the initialization of some variables*/
+#define NUMBER0  0    /* Initialize the accountant variable */
+#define NUMBER1  1    /* one of the conditions controls me */
+#define NUMBER2  2    /* one of the conditions controls me */
 
 int main(void)
 {
-int i, p;
+	unsigned int FirstDigit, SecondDigit,  StoredCharacter, Accountant;
 
-for (i = 0; i < 9; i++)
-{
-for (p = i + 1; p < 10; p++)
-{
-putchar(i + '0');
-putchar(p + '0');
-if (i != 8)
-{
-putchar(44);
-putchar(32);
-}
-}
-}
-return (0);
+	FirstDigit = VALUEINICIAL;
+	SecondDigit = VALUEINICIAL + 1;
+	Accountant = NUMBER0;
+	StoredCharacter = VALUEINICIAL;
+
+	while (StoredCharacter != BREAKWHILE)
+	{
+		putchar(StoredCharacter);
+
+		if (SecondDigit > NUMBER9)
+			FirstDigit++, SecondDigit = FirstDigit, SecondDigit++;
+
+		if (StoredCharacter >= '0' && StoredCharacter <= '9')
+			Accountant++;
+		if (Accountant == NUMBER1)
+			StoredCharacter = SecondDigit, SecondDigit++;
+		else if (Accountant == NUMBER2)
+			StoredCharacter = ',', Accountant = NUMBER0;
+		else if (StoredCharacter == ',')
+			StoredCharacter = ' ';
+		else if (StoredCharacter == ' ')
+			StoredCharacter = FirstDigit;
+
+		if (StoredCharacter == (BREAKWHILE - 1))
+			StoredCharacter++;
+		else if (FirstDigit == NUMBER9)
+			StoredCharacter = '\n';
+	}
+	return (0);
 }
