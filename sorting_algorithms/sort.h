c795a82fdc919f90a3b3b64acc57237b38c0e317
jordanbsandoval <1608@holbertonschool.com>
2021-02-04 22:23:07 -0500

    header

---
+++ sorting_algorithms/sort.h
@@ -0,0 +1,11 @@
+#ifndef sort_h
+#define sort_h
+#include<stdio.h>
+#include<stdlib.h>
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+
+#endif
