3d25658ac5a9d422b6d80e1c3d3f1fca32ba3646
jordanbsandoval <1608@holbertonschool.com>
2021-06-25 10:06:08 -0400

    debugging

--- 0x03-debugging/README.md
+++
@@ -1,9 +0,0 @@
-## 0x03 C - Debugging
-
-### General
-
-<ul>
-<li>What is debugging</li>
-<li>What are some methods of debugging manually</li>
-<li>How to read the error messages</li>
-</ul>
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,14 +1,23 @@
+#include "holberton.h"
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
-int main(void)
+/**
+ *positive_or_negative- determing if an number is positive or negative
+ *
+ *@n: Parametro to evalue
+ *
+ *Return: positive or negative
+ */
+
+void positive_or_negative(int n)
 {
-	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
 }
