42c49ddce5cfb1953e18e1d2e41ad0a0628e509c
jordanbsandoval <1608@holbertonschool.com>
2021-06-25 13:27:49 -0400

    function_multiplicated

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ *mul- function that multiplies two integers.
+ *@a: first parameter to evalue
+ *@b: second parameter to evalue
+ *
+ *Return: operation succesfull
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
