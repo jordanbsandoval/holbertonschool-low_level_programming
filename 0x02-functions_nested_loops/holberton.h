2b1254d63d81f34150b8574bc96c6be3425527f0
jordanbsandoval <1608@holbertonschool.com>
2021-06-24 23:28:37 -0400

    delete

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,8 +1,10 @@
-#if !defined(HDR)
-#define HDR
+#ifndef Holberton
+#define Holberton
+/* library's */
+#include <unistd.h>
 
+/* Prototypes */
 int _putchar(char c);
-void PrintCharacter(unsigned char ArrayOfCharacter[]);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -14,12 +16,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
-
-enum COLUMN
-{
-	COLUMN_ZERO, COLUMN_ONE, COLUMN_TWO, COLUMN_THREE,
-	COLUMN_FOUR, COLUMN_FIVE, NUMBER_OF_LINES = 1440
-};
 
 #endif
