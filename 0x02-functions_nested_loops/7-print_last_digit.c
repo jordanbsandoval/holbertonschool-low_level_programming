bc612bf0400a703d464e6859cec20a5fe5fd46d5
jordanbsandoval <1608@holbertonschool.com>
2021-06-25 01:23:11 -0400

    hotfix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,22 @@
 #include "holberton.h"
 
 /**
+ * _abs - show an int by stdout c
+ * print_last_digit - print last digit of a number
+ * @n: value
+ *
+ * Return: on (success) 1
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = n * -1;
+	return (n);
+
+}
+
+/**
  * print_last_digit - print last digit of a number
  * @n: value
  *
