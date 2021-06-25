52eead57319f13047df8b3cd669836827a9dbb36
jordanbsandoval <1608@holbertonschool.com>
2021-06-24 23:26:59 -0400

    alphabeto

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
+
 /**
- *print_alphabet- print alphabet lowercase
- *Return: exit succesful
+ *print_alphabet- print the alphabet, in lowercase, followed by new line
+ *Return: exit succesfull
  */
 
 void print_alphabet(void)
 {
-char c = 97;
+	int i;
 
-for (; c <= 122; c++)
-_putchar(c);
-
-_putchar('\n');
+	for (i = 97; i <= 122; i++)
+		_putchar(i);
+	_putchar('\n');
 }
