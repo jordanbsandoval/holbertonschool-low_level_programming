22385056eaac771f88debf50881af6331f13b677
jordanbsandoval <1608@holbertonschool.com>
2021-06-25 01:21:29 -0400

    last_digit_number

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdlib.h>
 
 /**
  *_abs- function that computes the absolute value of an integer.
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,19 @@
 #include "holberton.h"
 
 /**
+ * print_last_digit - print last digit of a number
+ * @n: value
  *
- *
+ * Return: on (success) 1
  */
 
 int print_last_digit(int n)
 {
-	int p;
-	p = n % 10;
-	return (p);
+	int r;
+
+	r = n % 10;
+	r = _abs(r);
+	_putchar(r + '0');
+
+	return (r);
 }
