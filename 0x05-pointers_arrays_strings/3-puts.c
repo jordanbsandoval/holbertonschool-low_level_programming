164fb7a048177472acdf2a3e362946c9bb9d1100
jordanbsandoval <1608@holbertonschool.com>
2021-07-02 19:17:10 -0400

    print-an-pointer

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *_puts: function that print a string
+ *@str: parameter pointer to char
+ *
+ *Return: print string
+ */
+
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+	_putchar('\n');
+}
