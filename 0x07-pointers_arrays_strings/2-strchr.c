d66e6611aa0cf6551379f7bbe62f5aa448a3e0cb
jordanbsandoval <1608@holbertonschool.com>
2021-07-29 20:49:12 -0400

    search-an-element-in-to-array

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
 
 /**
+ *_strchr- function that locates a character in a string.
  *
+ *@s: string to evaluate
+ *@c: character to search
  *
- *
- *
- *
+ *Return: position of the first ocurrenc
  */
 char *_strchr(char *s, char c)
 {
