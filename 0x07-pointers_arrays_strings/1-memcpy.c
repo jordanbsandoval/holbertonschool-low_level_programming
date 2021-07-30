28ba28d123bb184f106a2493389341fa33ca47c3
jordanbsandoval <1608@holbertonschool.com>
2021-07-29 20:27:21 -0400

    viendo-punteros-como-si-fueran-arrays

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,13 @@
 #include "holberton.h"
 
 /**
+ *_memcpy- function that copies memory area.
  *
+ *@dest: position of the array to changue of value
+ *@src: value to input in the array
+ *n: number of bytes to copy
  *
- *
- *
- *
+ *Return: pointer to init of array
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
