5d24e560d6f2d328b34921737eb3f3ab3cb22bb8
jordanbsandoval <1608@holbertonschool.com>
2021-07-30 12:22:21 -0400

    substring-with-pointer

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	while (*s)
+	{
+		i = 0;
+		while (accept[i])
+		{
+			if (*s == accept[i])
+				return (s);
+			i++;
+		}
+		s++;
+	}
+
+	return ('\0');
+}
