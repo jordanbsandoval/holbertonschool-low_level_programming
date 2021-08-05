6774a39f8e5f1fe62606cac6e73a5ecd83ce7f22
jordanbsandoval <1608@holbertonschool.com>
2021-08-05 12:46:25 -0400

    function-atoi-converter-string-in-int

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- *main- print the number of arguments passed into it.
+ *main- multiplies two numbers.
  *
  *@argc: number of elements passed into program
  *@argv: array of elements passed into it.
@@ -15,6 +15,7 @@ int main(int argc, char *argv[])
 	{
 		int num1 = atoi(argv[1]);
 		int num2 = atoi(argv[2]);
+
 		printf("%d\n", num1 * num2);
 	}
 	else
