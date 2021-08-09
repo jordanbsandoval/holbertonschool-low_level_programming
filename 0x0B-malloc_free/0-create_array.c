f9160c6cc3b8c5b04cf41e281eed78c1071f0e2d
jordanbsandoval <1608@holbertonschool.com>
2021-08-09 19:53:57 -0400

    malloc-create-dinamic-allocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,18 +14,21 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
-	char *tmp;
+	char *tmp = NULL;
 
 	if (size == 0)
+		return (0);
+
+	tmp = malloc(sizeof(char) * size);
+
+	if (tmp == NULL)
 		return (NULL);
-	else
-		tmp = malloc(sizeof(char) * size);
 
 	while (i < size)
 	{
 		tmp[i] = c;
 		i++;
 	}
-	*tmp = '\0';
+
 	return (tmp);
 }
