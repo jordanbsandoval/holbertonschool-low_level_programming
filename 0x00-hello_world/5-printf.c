bf3190a19941ea4170e199aaa0086c706c2b07f3
jordanbsandoval <1608@holbertonschool.com>
2021-09-01 17:51:39 -0500

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
-*main - Entry point
-*
-*Return: always 0 (success)
-*/
+ *main- usando la funcion printf
+ *
+ *Return: Always 0.
+ */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
-
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ *main- Programa que imprime el tamanio de varios tipos de datos en el computador que se ejecuta y se compila
  *
- * Return: Always 0 (success)
+ *Return: Always 0.
  */
+
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(double));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long: %d byte(s)\n", sizeof(long long));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
+
 	return (0);
 }
-
