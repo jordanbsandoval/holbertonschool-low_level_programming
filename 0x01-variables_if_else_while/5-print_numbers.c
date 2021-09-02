b59f2966e6ddf184518f5655e194f60a8703995d
jordanbsandoval <1608@holbertonschool.com>
2021-09-01 23:31:51 -0500

    print_number_base10

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+int print_num_base10(void);
 /**
  *main- print all single digit numbers of base 10 starting from 0
  *
@@ -7,6 +8,16 @@
  */
 
 int main(void)
+{
+	print_num_base10();
+	return (0);
+}
+/**
+ *print_num_base10- imprime los numeros de base 10 empezando con el 0
+ *
+ *Return: always 0.
+ */
+int print_num_base10(void)
 {
 	int i = 0;
 
