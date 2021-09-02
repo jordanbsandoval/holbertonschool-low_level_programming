cfe7e7d26e3df971e33572872910ba1b0999f501
jordanbsandoval <1608@holbertonschool.com>
2021-09-01 22:48:34 -0500

    print-reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+void print_str(char s[]);
+int str_len(char s[]);
+/**
+ *str_len- devuelve el numero de elementos en el array
+ *
+ *@s: string to count
+ *
+ *Return: number elementos del array
+ */
+int str_len(char s[])
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+/**
+ *print_str- imprimer el string pasado
+ *
+ *@s: string to print
+ *
+ *Return: always 0.
+ */
+void print_str(char s[])
+{
+	int i = 0;
+
+	i = str_len(s);
+	i--;
+
+	while (s[i] != '\0')
+	{
+		putchar(s[i]);
+		i--;
+	}
+}
+/**
+ *main- programa que imprime el abecedario en minuscula empezando por la z-a
+ *
+ *Return: always 0.
+ */
+
+int main(void)
+{
+	char alp[] = "abcdefghijklmnopqrstuvwxyz";
+
+	print_str(alp);
+	putchar('\n');
+	return (0);
+}
