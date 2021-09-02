a25474b1bcd6bb05b652966590aab3761f4b70fa
jordanbsandoval <1608@holbertonschool.com>
2021-09-01 19:22:25 -0500

    delete

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-char p[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-write(1, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", sizeof(p));
-return (1);
+        char p[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+        write(1, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", sizeof(p));
+        return (1);
 }
