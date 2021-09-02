1b3d2f48b06adebcb1b2e835ff83486adccea7c4
jordanbsandoval <1608@holbertonschool.com>
2021-09-01 23:37:46 -0500

    putchar_num_base10

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+int putchar_num_base10(void);
 /**
  *main- print all single digit numbers of base 10 starting from 0
  *
@@ -7,6 +8,16 @@
  */
 
 int main(void)
+{
+	putchar_num_base10();
+	return (0);
+}
+/**
+ *putchar_num_base10- imprime los numeros de base 10 empezando con el 0
+ *
+ *Return: always 0.
+ */
+int putchar_num_base10(void)
 {
 	int i = 0;
 
