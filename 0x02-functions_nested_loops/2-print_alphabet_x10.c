9245253490f8fd11e91a65a9c2ae7d65411efcd1
jordanbsandoval <1608@holbertonschool.com>
2021-09-02 11:34:23 -0500

    print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,3 +12,18 @@ void print_alphabet_x10(void)
 	for (i = 0; i < 10; i++)
 		print_alphabet();
 }
+/**
+ *print_alphabet- programa que imprime el abecedario con la funcion propia _put
+ *
+ *Return: always 0.
+ */
+
+void print_alphabet(void)
+{
+	int i = 0;
+	char alp[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (; alp[i] != '\0'; i++)
+		_putchar(alp[i]);
+	_putchar('\n');
+}
