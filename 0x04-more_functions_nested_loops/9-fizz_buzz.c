5bec6f6bf84907cc5af8021a8f4f92df4cd78ad8
jordanbsandoval <1608@holbertonschool.com>
2021-09-14 23:21:58 -0500

    program-fizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main- programa fizz buzz que imprime los numeros del 1-100
+ *
+ *Return: imprime los numeros del 1-100 con algunas condiciones para reemplazar
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
