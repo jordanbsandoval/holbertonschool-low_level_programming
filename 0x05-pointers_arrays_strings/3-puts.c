8ab255c05e204896e299746e50c3aa284c37593a
jordanbsandoval <1608@holbertonschool.com>
2021-09-15 17:45:28 -0500

    puntero_a_char_imprime_string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
+ *_puts- imprime lo que esta apuntando en el parametro pasado como argumento
  *
+ *@str: parametro apuntado tipo char
  *
- *
+ *Return: imprime lo que apunta el parametro
  */
 
 void _puts(char *str)
