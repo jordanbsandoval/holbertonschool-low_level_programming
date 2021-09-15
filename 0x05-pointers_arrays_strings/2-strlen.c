2764fbe54534c2302d41491afdef7c70fc805b8a
jordanbsandoval <1608@holbertonschool.com>
2021-09-15 17:27:32 -0500

    pointer-string-length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_strlen- determina el ancho de un string
+ *
+ *@s: puntero a un string
+ *
+ *Return: numer de caracteres del string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s++)
+		i++;
+	return (i);
+}
