26bdb5a125237ddd0a0907f9dd88fb263406fd83
jordanbsandoval <1608@holbertonschool.com>
2021-09-15 17:20:55 -0500

    punteros

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *reset_to_98- cambia el valor de la variable apuntada
+ *
+ *@n: puntero a una variable tipo entero
+ *
+ *Return: cambia el valor
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
