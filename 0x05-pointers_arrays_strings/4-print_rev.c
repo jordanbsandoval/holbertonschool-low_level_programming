860881dc46a9277065dc205056ab2bdf40e4386d
jordanbsandoval <1608@holbertonschool.com>
2021-09-15 17:54:34 -0500

    print_string_in_reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *print_rev- prints a string, in reverse, followed by a new line
+ *
+ *@s: puntero a char
+ *
+ *Return: string in reverse
+ */
+
+void print_rev(char *s)
+{
+	int ancho = _strlen(s);
+
+	for (; ancho >= 0; ancho--)
+		printf("%c", s[ancho]);
+	printf("\n");
+}
+
+/**
+ *_strlen- determina el ancho de un string
+ *
+ *@s: puntero a un string
+ *
+ *Return: numer de caracteres del string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	return (i);
+}
