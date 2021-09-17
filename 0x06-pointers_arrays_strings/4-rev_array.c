8337fc73fb7b849ea4544372900ba8786b5baad1
jordanbsandoval <1608@holbertonschool.com>
2021-09-17 00:20:45 -0500

    reverse-an-array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *reverse_array- reversa los elementos de un array
+ *
+ *@a: puntero a un array de punteros
+ *@n: numero de elementos en el array
+ *
+ *Return: imprime el array reversado
+ */
+
+void reverse_array(int *a, int n)
+{
+	int tmp;
+	int i = 0;
+	int ult_pos = 0;
+	int stop = n / 2;
+
+	while (n >= stop)
+	{
+		ult_pos = n - 1;
+		tmp = a[ult_pos];
+		a[ult_pos] = a[i];
+		a[i] = tmp;
+		n--, i++;
+	}
+}
