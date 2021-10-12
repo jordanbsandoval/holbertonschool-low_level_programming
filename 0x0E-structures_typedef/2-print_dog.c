994a02b6dd1df274cecdd3aa340e3d72874b0068
jordanbsandoval <1608@holbertonschool.com>
2021-10-12 11:16:25 -0500

    structurs-validating-value-of-each-element

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,25 +1,31 @@
-#include <stdio.h>
 #include "dog.h"
 
 /**
- * print_dog - prints the values os a structure
- * @d: pointer to the structure
+ *
+ *
+ *
+ *
  *
  */
+
 void print_dog(struct dog *d)
 {
-	if (d)
+	if (d == NULL)
+		printf("Nothing");
+	else
 	{
-		if (d->name  == NULL)
+		if (d->name == NULL)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
-
-		printf("Age: %.6f\n", d->age);
-
+		if (d->age < 0)
+			printf("Age: (nil)\n");
+		else
+			printf("Name: %f\n", d->age);
 		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
 		else
-			printf("Owner: %s\n", d->owner);
+			printf("Name: %s\n", d->owner);
+
 	}
 }
