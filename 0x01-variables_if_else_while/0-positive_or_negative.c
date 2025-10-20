ab21448aa9ec5dd9ea5ef5e6e039fbb45f07ef8c
jordanbsandoval <1608@holbertonschool.com>
2025-10-20 14:06:48 -0500

    if-else-elseif

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,31 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
 /**
- *main- Programa que genera un numero random y aplica condiciones para imprimir
- *
- *Return: always 0.
- */
+* main- Este programa asignará un número aleatorio a la variable n
+* cada vez que se ejecute.
+* Complete el código fuente para indicar si el número almacenado
+* en la variable n es positivo o negativo.
+*
+* Return: (0)
+*/
 
 int main(void)
 {
-	int n;
+  int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else
-		printf("%d is zero\n". n);
-	return (0);
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+
+  if (n < 0)
+    printf("el numero %d is negative\n", n);
+
+  else if (n > 0)
+    printf("el numero %d is positive\n", n);
+
+  else
+    printf("el numero %d is zero\n", n);
+
+  return (0);
 }
