6cd0402b56b889643a0294f563d450dbb442a4c0
jordanbsandoval <1608@holbertonschool.com>
2025-10-22 16:15:05 -0500

    simpleCode

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,27 +1,18 @@
 #include "main.h"
 
 /**
- *print_to_98- imprime todos los numeros naturales hasta el 98
- *
- *@n: parametro inicial
- *
- *Return: always 0.
- */
+* print_to_98- función que imprima todos los números naturales desde n hasta90
+*              Seguido de una nueva linea
+* 
+* Return: print number to 98.
+*/
 
 void print_to_98(int n)
 {
-	int stop = 98;
-
-	if (n <= stop)
-		for (; n <= stop; n++)
-		{
-			printf("%d", n);
-			n != stop ? printf(", ") : printf("\n");
-		}
-	else
-		for (; n >= stop; n--)
-		{
-			printf("%d", n);
-			n != stop ? printf(", ") : printf("\n");
-		}
+  if (n <= 98)
+    for (; n <= 98; n++)
+      (n != 98) ? printf("%d, ", n) : printf("%d\n", n);
+  else
+    for (; n >= 98; n--)
+      (n != 98) ? printf("%d, ", n) : printf("%d\n", n);
 }
